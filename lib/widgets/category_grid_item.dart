efdcd6cce867c3f612d4b310b6182423ec8d5c2b
al-masud007 <almasudsakib@gmail.com>
2026-01-26 20:41:56 +0600

    Categories shape upgraded

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -9,24 +9,31 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            category.color.withValues(alpha: .55),
-            category.color.withValues(alpha: .9),
-          ],
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
 
-          begin: AlignmentGeometry.topLeft,
-          end: AlignmentGeometry.bottomRight,
+      child: Container(
+        padding: EdgeInsets.all(24),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withValues(alpha: .55),
+              category.color.withValues(alpha: .9),
+            ],
+
+            begin: AlignmentGeometry.topLeft,
+            end: AlignmentGeometry.bottomRight,
+          ),
         ),
-      ),
 
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-          color: Theme.of(context).colorScheme.onSurface,
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+            color: Theme.of(context).colorScheme.onSurface,
+          ),
         ),
       ),
     );
