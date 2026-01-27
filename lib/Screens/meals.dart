58d878435977ccfc2b8b6d0bdac03fe87db6862f
al-masud007 <almasudsakib@gmail.com>
2026-01-27 19:58:56 +0600

    showing meals on selected category

--- lib/Screens/meals.dart
+++ lib/Screens/meals.dart
@@ -3,6 +3,7 @@ import 'package:meals/main.dart';
 import 'package:meals/model/meal.dart';
 
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
@@ -42,7 +43,7 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (ctx, index) => Text(meals[index].title),
+        itemBuilder: (ctx, index) => MealItem(meal: meals[index]),
       );
     }
 
---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:meals/model/meal.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class MealItem extends StatelessWidget {
+  const MealItem({super.key, required this.meal});
+
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: InkWell(
+        onTap: () {},
+        child: Stack(
+          children: [
+            FadeInImage(
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+            ),
+            Positioned(
+              top: 0,
+              bottom: 0,
+              left: 0,
+              child: Container(
+                color: Color.fromARGB(138, 0, 0, 0),
+                padding: EdgeInsets.symmetric(horizontal: 44, vertical: 6),
+                child: Column(
+                  children: [
+                    Text(
+                      meal.title,
+                      maxLines: 2,
+                      textAlign: TextAlign.center,
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    Row(children: [
+                        
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
