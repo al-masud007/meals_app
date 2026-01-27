70314de3f05a28d0799ce72364c201d7f3a4586d
al-masud007 <almasudsakib@gmail.com>
2026-01-27 20:12:47 +0600

    fixed image issue

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -10,6 +10,12 @@ class MealItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      margin: const EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      clipBehavior: Clip.hardEdge,
+      elevation: 2,
       child: InkWell(
         onTap: () {},
         child: Stack(
@@ -17,11 +23,14 @@ class MealItem extends StatelessWidget {
             FadeInImage(
               placeholder: MemoryImage(kTransparentImage),
               image: NetworkImage(meal.imageUrl),
+              fit: BoxFit.cover,
+              height: 200,
+              width: double.infinity,
             ),
             Positioned(
-              top: 0,
               bottom: 0,
               left: 0,
+              right: 0,
               child: Container(
                 color: Color.fromARGB(138, 0, 0, 0),
                 padding: EdgeInsets.symmetric(horizontal: 44, vertical: 6),
